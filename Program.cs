711146de094c365e950ee702766a44848ecd6d08
Hugo Obuchi <76051977+hugo-obuchi@users.noreply.github.com>
2021-07-31 23:19:55 +0900

    Initial GameLift Server code and lib commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AGLW_CSharp_CustomServerBasicSample
+{
+    class Program
+    {
+        static private GameServer server = null;
+
+        static void Main(string[] args)
+        {
+            server = new GameServer();
+            server.Start();
+
+            while(server.IsAlive)
+            {
+
+            }
+
+            
+        }
+    }
+}
